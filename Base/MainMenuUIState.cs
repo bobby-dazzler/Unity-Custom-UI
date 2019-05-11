f0a0b566d768f297045e1fe745af642a25a674be
bobby-dazzler <philipanderson@outlook.com>
2019-05-11 13:42:57 +0100

    version 0.0.1

---
+++ Base/MainMenuUIState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEventsFramework;
+
+[CreateAssetMenu(menuName="Assets/UI/Main Menu State")]
+public class MainMenuUIState : State {
+    
+}
--- Base/UiInitializer.cs
+++ Base/UiInitializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEventsFramework;
+using UnityEngine.UI;
 
 public class UiInitializer : MonoBehaviour {
 
@@ -9,14 +10,23 @@ public class UiInitializer : MonoBehaviour {
 
 	public UIRuntimeSet allUIStateRootGameObjects;
 
-	//public UIRuntimeSet menusAvaialbleFromGridGen;
+	public WaveDataRuntimeSet allWaveData;
+
+	public Transform waveAParent;
+
+	public Transform waveBParent;
+
+	public Transform waveCParent;
 
 	void Awake () {
-		StateController controller = GetComponent<StateController>();
+/* 		StateController controller = GetComponent<StateController>();
 		controller.currentState = defaultState;
 		allUIStateRootGameObjects.controller = controller;
 		
-		ConfigureMenus();
+		ConfigureMenus(); */
+
+		ConfigureWaveData();
+		SetWaveSliderValues();
 	}
 
 	void ConfigureMenus () {
@@ -34,5 +44,42 @@ public class UiInitializer : MonoBehaviour {
 		allUIStateRootGameObjects.items[0].SetActive(true);
 		allUIStateRootGameObjects.activeUI = allUIStateRootGameObjects.items[0];
 		
+	}
+
+	void ConfigureWaveData() {
+		allWaveData.items[0].waveXSlider = waveAParent.Find("X Slider").GetComponent<Slider>();
+		allWaveData.items[0].waveYSlider = waveAParent.Find("Y Slider").GetComponent<Slider>();
+		allWaveData.items[0].waveSteepnessSlider = waveAParent.Find("Wave Height Slider").GetComponent<Slider>();
+		allWaveData.items[0].waveLengthSlider = waveAParent.Find("Wave Length Slider").GetComponent<Slider>();
+		
+		allWaveData.items[1].waveXSlider = waveBParent.Find("X Slider").GetComponent<Slider>();
+		allWaveData.items[1].waveYSlider = waveBParent.Find("Y Slider").GetComponent<Slider>();
+		allWaveData.items[1].waveSteepnessSlider = waveBParent.Find("Wave Height Slider").GetComponent<Slider>();
+		allWaveData.items[1].waveLengthSlider = waveBParent.Find("Wave Length Slider").GetComponent<Slider>();
+
+		allWaveData.items[2].waveXSlider = waveCParent.Find("X Slider").GetComponent<Slider>();
+		allWaveData.items[2].waveYSlider = waveCParent.Find("Y Slider").GetComponent<Slider>();
+		allWaveData.items[2].waveSteepnessSlider = waveCParent.Find("Wave Height Slider").GetComponent<Slider>();
+		allWaveData.items[2].waveLengthSlider = waveCParent.Find("Wave Length Slider").GetComponent<Slider>();
+	}
+
+	public void SetWaveSliderValues(){
+
+		// Set the slider to the current values
+		allWaveData.items[0].waveXSlider.value = allWaveData.items[0].wave.x;
+		allWaveData.items[0].waveYSlider.value = allWaveData.items[0].wave.y;
+		allWaveData.items[0].waveSteepnessSlider.value = allWaveData.items[0].wave.z;
+		allWaveData.items[0].waveLengthSlider.value = allWaveData.items[0].wave.w;
+
+		allWaveData.items[1].waveXSlider.value = allWaveData.items[1].wave.x;
+		allWaveData.items[1].waveYSlider.value = allWaveData.items[1].wave.y;
+		allWaveData.items[1].waveSteepnessSlider.value = allWaveData.items[1].wave.z;
+		allWaveData.items[1].waveLengthSlider.value = allWaveData.items[1].wave.w;
+
+		allWaveData.items[2].waveXSlider.value = allWaveData.items[2].wave.x;
+		allWaveData.items[2].waveYSlider.value = allWaveData.items[2].wave.y;
+		allWaveData.items[2].waveSteepnessSlider.value = allWaveData.items[2].wave.z;
+		allWaveData.items[2].waveLengthSlider.value = allWaveData.items[2].wave.w; 
+
 	}
 }
